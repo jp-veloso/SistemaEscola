92609ddf825fa35d5f6de1ab235cce52a666f29a
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-10 14:34:34 -0300

    Ações adicionado

---
+++ acoes.php
@@ -0,0 +1,17 @@
+<?php
+include_once "seguranca.php";
+include_once "conexao.php";
+$acao = $_GET["acao"];
+
+// Inserir dados de usuarios
+if ($acao == 1) {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "insert into usuarios (nome,email,senha) values ('$nome','$email','$senha')";
+    $conexao = new conexao();
+    $conexao->executar($sql);
+    header("location: index.php?acao=1");
+    die();
+}
