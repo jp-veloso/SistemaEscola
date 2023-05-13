8831b8c4f4a0a9242871d484a63d6c19f9eba443
Mesquita Dev <lmesquita2k@gmail.com>
2023-05-13 14:22:08 -0300

    Adicionando CSS cadastro de curso

--- cadastroCurso.php
+++ cadastroCurso.php
@@ -12,20 +12,20 @@ $conexao = new conexao();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro curso</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <h1>Cadastro de cursos</h1>
-    <hr>
-    <form action="acoes.php?acao=2" method="POST">
-        <label>Nome:</label>
-        <input type="text" name="nome">
-        <br>
-        <label>Descrição:</label>
-        <input type="text" name="descricao">
-        <br>
-        <input type="submit" value="Enviar">
-    </form>
+<body class="flex justify-center items-center h-screen bg-gray-900">
+    <div class="max-w-xs p-12 bg-gray-200 border border-indigo-800 rounded-sm">
+        <h1 class="text-2xl font-medium mb-4 font-['Inter']">Cadastrar curso</h1>
+        <form action="acoes.php?acao=2" method="POST" class="flex flex-col">
+            <label class="inline-block mb-1">Nome:</label>
+            <input type="text" name="nome" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400 mb-2" placeholder="Nome do curso">
+            <label class="inline-block mb-1">Descrição:</label>
+            <input type="text" name="descricao" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400 mb-2" placeholder="Tecnologia, saúde...">
+            <input type="submit" value="Enviar" class="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 transition duration-200 rounded-lg text-white mt-4">
+        </form>
+    </div>
 </body>
 
 </html>
