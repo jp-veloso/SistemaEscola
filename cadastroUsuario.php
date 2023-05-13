d06ecfab9914f40101f024504c1b3f234607da11
Mesquita Dev <lmesquita2k@gmail.com>
2023-05-13 12:35:36 -0300

    Adicionando CSS cadastro usuário

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -10,23 +10,25 @@ include_once "conexao.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <h2>Tela de Cadastro</h2>
-    <hr>
-    <form action="acoes.php?acao=1" method="POST">
-        <label>Nome:</label>
-        <input type="text" name="nome" />
-        <br />
-        <label>E-mail:</label>
-        <input type="email" name="email" />
-        <br />
-        <label>Senha:</label>
-        <input type="password" name="senha" />
-        <br />
-        <input type="submit" value="Enviar Dados">
-    </form>
+<body class="flex justify-center items-center h-screen bg-gray-900">
+    <div class="max-w-xs p-12 bg-gray-200 border border-indigo-800 rounded-sm">
+        <h2 class="text-2xl font-medium mb-4 font-['Inter']">Tela de Cadastro</h2>
+        <form action="acoes.php?acao=1" method="POST" class="flex flex-col">
+            <label class="inline-block mb-1">Nome:</label>
+            <input type="text" name="nome" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400 mb-2" placeholder="Seu nome"/>
+            <label class="inline-block mb-1">E-mail:</label>
+            <input type="email" name="email" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400 mb-2" placeholder="Seu email"/>
+            <label class="inline-block">Senha:</label>
+            <input type="password" name="senha" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400 mb-2" placeholder="Sua senha"/>
+            <div class="flex flex-col">
+                <input type="submit" value="Enviar Dados" class="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 transition duration-200 rounded-lg text-white mt-4">
+                <a href="./index.php" class="text-center mt-2 text-neutral-600">Voltar para login</a>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -9,15 +9,15 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="flex justify-center items-center h-screen bg-gray-900 font-sans">
+<body class="flex justify-center items-center h-screen bg-gray-900">
     <div class="max-w-xs p-12 bg-gray-200 border border-indigo-800 rounded-sm">
         <h1 class="text-2xl font-medium mb-4 font-['Inter']">Sistema Escolar</h1>
-        <form action="validacaoLogin.php" method="POST" class="flex flex-col gap-0">
+        <form action="validacaoLogin.php" method="POST" class="flex flex-col">
             <label class="inline-block mb-1">E-mail: </label>
             <input type="email" name="email" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400 mb-2" placeholder="Seu email"/>
             <label class="inline-block mb-1">Senha: </label>
             <input type="password" name="senha" class="border border-gray-300 rounded py-2 px-4 w-full focus:outline-none focus:border-indigo-400" placeholder="Sua senha"/>
-            <div class="flex gap-x-4 mt-6">
+            <div class="flex justify-between mt-6">
                 <input type="submit" value="Entrar" class="px-4 py-2 bg-indigo-600 hover:bg-indigo-700 transition duration-200 rounded-lg text-white"/>
                 <a href="cadastroUsuario.php" class="px-4 py-2 border border-indigo-300 transition duration-200 rounded-lg">Cadastrar</a>
             </div>
