1c3bc7e3bdf90c75f8da93d5349a28c696183b3e
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-15 15:08:30 -0300

    Ajuste css tabela

--- visualizarCursos.php
+++ visualizarCursos.php
@@ -15,48 +15,48 @@ $conexao = new conexao();
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="flex justify-center items-center h-screen bg-gray-900">
-    <div class="bg-gray-200 border border-indigo-800 rounded-sm border-8">
-        <h1 class=" text-2xl font-medium mb-4 font-['Inter']">Visualização de cursos</h1>
-        <a href="cadastroCurso.php">Cadastrar novo curso</a>
+<body class="bg-gray-100 text-gray-800 font-sans leading-normal p-10">
+    <div class="container mx-auto">
+        <h1 class="text-3xl font-bold mb-4">Visualização de cursos</h1>
+        <a class="text-blue-500 hover:text-blue-700" href="cadastroCurso.php">Cadastrar novo curso</a>
         <br>
-        <a href="cadastroDisciplina.php">Cadastrar nova disciplina</a>
+        <a class="text-blue-500 hover:text-blue-700" href="cadastroDisciplina.php">Cadastrar nova disciplina</a>
         <br>
         <br>
-        <table class="border-collapse border border-slate-500">
+        <table class="min-w-full border border-gray-300">
             <?php
             $arrCursos = $conexao->executar("select * from cursos");
             foreach ($arrCursos as $cursos) {
             ?>
                 <tr>
-                    <th class="border border-slate-600">ID</th>
-                    <th class="border border-slate-600">CURSO</th>
-                    <th class="border border-slate-600">DESCRIÇÃO</th>
-                    <th class="border border-slate-600">ALTERAR</th>
-                    <th class="border border-slate-600">EXCLUIR</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">ID</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">CURSO</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">DESCRIÇÃO</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">ALTERAR</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">EXCLUIR</th>
                 </tr>
                 <tr>
-                    <th class="border border-slate-600"><?= $cursos['id'] ?></th>
-                    <th class="border border-slate-600"><?= $cursos['nome'] ?></th>
-                    <th class="border border-slate-600"><?= $cursos['descricao'] ?></th>
-                    <th class="border border-slate-600">
-                        <a href=" alterarCurso.php?id=<?= $cursos['id'] ?>">Alterar</a>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100 font-normal"><?= $cursos['id'] ?></th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100 font-normal"><?= $cursos['nome'] ?></th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100 font-normal"><?= $cursos['descricao'] ?></th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">
+                        <a class="text-blue-500 hover:text-blue-700" href=" alterarCurso.php?id=<?= $cursos['id'] ?>">Alterar</a>
                     </th>
-                    <th class="border border-slate-600">
-                        <a href="acoes.php?id=<?= $cursos['id'] ?>&acao=3">Excluir</a>
-                    </th class="border border-slate-600">
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">
+                        <a class="text-blue-500 hover:text-blue-700" href="acoes.php?id=<?= $cursos['id'] ?>&acao=3">Excluir</a>
+                    </th class="py-2 px-4 border-b border-gray-300 bg-gray-100">
                 </tr>
                 <tr>
-                    <th class="border border-slate-600">ID</th>
-                    <th class="border border-slate-600">DISCIPLINAS</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">ID</th>
+                    <th class="py-2 px-4 border-b border-gray-300 bg-gray-100">DISCIPLINAS</th>
                 </tr>
                 <?php
                 $arrDisciplinas = $conexao->executar("select * from disciplinas where id_curso=$cursos[id]");
                 foreach ($arrDisciplinas as $disciplinas) {
                 ?>
                     <tr>
-                        <td class="border border-slate-700"><?= $disciplinas['id'] ?></td>
-                        <td class="border border-slate-700"><?= $disciplinas['nome'] ?></td>
+                        <th class="py-2 px-4 border-b border-gray-300 bg-gray-100 font-normal"><?= $disciplinas['id'] ?></th>
+                        <th class="py-2 px-4 border-b border-gray-300 bg-gray-100 font-normal"><?= $disciplinas['nome'] ?></th>
                     </tr>
                 <?php
                 }
