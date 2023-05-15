51d42fcc5f7531d8c5cb6818fe3be6ea7dca160f
Marco Tulio <marcotulio_iub@hotmail.com>
2023-05-15 14:42:05 -0300

    Css tela de visuzalição

--- visualizarCursos.php
+++ visualizarCursos.php
@@ -12,58 +12,59 @@ $conexao = new conexao();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <h2>Visualização de cursos</h2>
-    <hr />
-    <br>
-    <a href="cadastroCurso.php">Cadastrar novo curso</a>
-    <br>
-    <a href="cadastroDisciplina.php">Cadastrar nova disciplina</a>
-    <br>
-    <br>
-    <table>
-        <?php
-        $arrCursos = $conexao->executar("select * from cursos");
-        foreach ($arrCursos as $cursos) {
-        ?>
-            <tr>
-                <th>ID</th>
-                <th>CURSO</th>
-                <th>DESCRIÇÃO</th>
-                <th>ALTERAR</th>
-                <th>EXCLUIR</th>
-            </tr>
-            <tr>
-                <th><?= $cursos['id'] ?></th>
-                <th><?= $cursos['nome'] ?></th>
-                <th><?= $cursos['descricao'] ?></th>
-                <th>
-                    <a href="alterarCurso.php?id=<?= $cursos['id'] ?>">Alterar</a>
-                </th>
-                <th>
-                    <a href="acoes.php?id=<?= $cursos['id'] ?>&acao=3">Excluir</a>
-                </th>
-            </tr>
-            <tr>
-                <th>ID</th>
-                <th>DISCIPLINAS</th>
-            </tr>
+<body class="flex justify-center items-center h-screen bg-gray-900">
+    <div class="bg-gray-200 border border-indigo-800 rounded-sm border-8">
+        <h1 class=" text-2xl font-medium mb-4 font-['Inter']">Visualização de cursos</h1>
+        <a href="cadastroCurso.php">Cadastrar novo curso</a>
+        <br>
+        <a href="cadastroDisciplina.php">Cadastrar nova disciplina</a>
+        <br>
+        <br>
+        <table class="border-collapse border border-slate-500">
             <?php
-            $arrDisciplinas = $conexao->executar("select * from disciplinas where id_curso=$cursos[id]");
-            foreach ($arrDisciplinas as $disciplinas) {
+            $arrCursos = $conexao->executar("select * from cursos");
+            foreach ($arrCursos as $cursos) {
             ?>
                 <tr>
-                    <td><?= $disciplinas['id'] ?></td>
-                    <td><?= $disciplinas['nome'] ?></td>
+                    <th class="border border-slate-600">ID</th>
+                    <th class="border border-slate-600">CURSO</th>
+                    <th class="border border-slate-600">DESCRIÇÃO</th>
+                    <th class="border border-slate-600">ALTERAR</th>
+                    <th class="border border-slate-600">EXCLUIR</th>
                 </tr>
+                <tr>
+                    <th class="border border-slate-600"><?= $cursos['id'] ?></th>
+                    <th class="border border-slate-600"><?= $cursos['nome'] ?></th>
+                    <th class="border border-slate-600"><?= $cursos['descricao'] ?></th>
+                    <th class="border border-slate-600">
+                        <a href=" alterarCurso.php?id=<?= $cursos['id'] ?>">Alterar</a>
+                    </th>
+                    <th class="border border-slate-600">
+                        <a href="acoes.php?id=<?= $cursos['id'] ?>&acao=3">Excluir</a>
+                    </th class="border border-slate-600">
+                </tr>
+                <tr>
+                    <th class="border border-slate-600">ID</th>
+                    <th class="border border-slate-600">DISCIPLINAS</th>
+                </tr>
+                <?php
+                $arrDisciplinas = $conexao->executar("select * from disciplinas where id_curso=$cursos[id]");
+                foreach ($arrDisciplinas as $disciplinas) {
+                ?>
+                    <tr>
+                        <td class="border border-slate-700"><?= $disciplinas['id'] ?></td>
+                        <td class="border border-slate-700"><?= $disciplinas['nome'] ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
             <?php
             }
             ?>
-        <?php
-        }
-        ?>
+    </div>
 </body>
 
 </html>
